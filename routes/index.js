cd693ed66ddd44d41cd6cbff37b1b894ea1da8a1
hyun-stone <hyun698207@gmail.com>
2024-07-07 11:59:40 +0900

    12

--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,37 @@ var router = Router();
 
 router.get("/", function(req, res, next){
   res.render('index', {data : {
-    typoResult : [
-        {
-            name : "torch",
-            score : 4.5,
-            danger : "alert",
-            result : "설명설명설명설명"
-        },
-        {
-            name : "torch",
-            score : 4.5,
-            danger : "alert",
-            result : "설명설명설명설명"
-        },
-        {
-            name : "torch",
-            score : 4.5,
-            danger : "alert",
-            result : "설명설명설명설명"
-        },
-    ],
-    recommand : "pip install torch222"
+    typoResult : [],
+    recommand : ""
 }});
 })
+
+router.get('/check', (req, res) => {
+    console.log(req.query);
+    res.render('index', {data : {
+        typoResult : [
+            {
+                name : "torch",
+                score : 4.5,
+                danger : "alert",
+                result : "설명설명설명설명"
+            },
+            {
+                name : "torch",
+                score : 4.5,
+                danger : "alert",
+                result : "설명설명설명설명"
+            },
+            {
+                name : "torch",
+                score : 4.5,
+                danger : "alert",
+                result : "설명설명설명설명"
+            },
+        ],
+        recommand : "pip install torch222"
+    }});
+});
 
 
 export default router;
