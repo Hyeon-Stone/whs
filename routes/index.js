192b09a941a451ce7714fac6f6b4af26485f7ec1
hyun-stone <hyun698207@gmail.com>
2024-07-07 13:48:26 +0900

    123

--- routes/index.js
+++ routes/index.js
@@ -10,31 +10,37 @@ router.get("/", function(req, res, next){
 })
 
 router.get('/check', (req, res) => {
-    console.log(req.query);
-    res.render('index', {data : {
-        typoResult : [
-            {
-                name : "torch",
-                score : 4.5,
-                danger : "alert",
-                result : "설명설명설명설명"
-            },
-            {
-                name : "torch",
-                score : 4.5,
-                danger : "alert",
-                result : "설명설명설명설명"
-            },
-            {
-                name : "torch",
-                score : 4.5,
-                danger : "alert",
-                result : "설명설명설명설명"
-            },
-        ],
-        recommand : "pip install torch222"
-    }});
+    
+    res.render('index', {data : JSON.parse(solveTypo)});
 });
+
+const solveTypo = (packageName) => {
+    return new Promise((resolve, reject) => {
+        try{
+            const result = spawn(
+                'python3',
+                ['./util/myproject/main.py', packageName, "--clean"],
+            );
+            let resultJsonString
+            result.stdout.on('data', (data) => {
+                // 파이썬 출력으로부터 캡차 문자열 저장
+                resultJsonString = data.toString().replace(/\n/g, "");
+            });
+            // result.stderr.on('data', (data) => {
+            //     console.log(`child stderr 여기: ${data}`);
+            // });
+            result.on('close', (data) => {
+                // 생성했던 폴더 및 파일 삭제
+                // 캡차 문자열 resolve
+                resolve(resultJsonString) 
+            });
+        } catch (err) {
+            console.log(err);
+            reject(err);
+        }
+    })
+
+}
 
 
 export default router;
